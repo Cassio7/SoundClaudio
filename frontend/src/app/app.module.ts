b7267d6dd3a1cb3ee2f4827102abfc733518cd46
Cassio7 <Cassio7@users.noreply.github.com>
2023-07-08 19:07:21 +0200

    update db + player

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
--- table.sql
+++ table.sql
@@ -7,4 +7,16 @@ create table users(
     UNIQUE (email)
 );
 
+create table songs(
+    id int primary key AUTO_INCREMENT,
+    name varchar(250),
+    artist_name varchar(250),
+    id_user int,
+    durata float,
+    mp3 varchar(250),
+    FOREIGN KEY (id_user) REFERENCES users(id)
+);
+
 insert into users(name,email,password,admin) value('Admin','admin@soundclaudio.it','password','1');
+
+insert into songs(name,artist_name,durata,mp3) value('Working Out','JID','3.46','D:\\Angular\\soundclaudio\\mp3\\Workin_Out.mp3');
