8a6d85a5b2b709b05cbad8d4429f3a67af40a192
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-09 18:25:12 +0200

    comments

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-
+// Cross-Origin Resource Sharing ... specifies which domains access your resources
 var cors = require('cors');
 
+//web application framework that provides features for web applications
 const express = require('express');
 
+// execute the connection
 const connection = require('./connection');
+
 const usersRoute = require('./routes/users');
+
+//create an instance of the Express, allowing to define and handle routes
 const app = express();
 
+// get the response of this localhost server
 app.use(cors());
+
+// parses incoming requests with URL-encoded payloads
 app.use(express.urlencoded({extended: true}));
+
+// parses incoming requests with JSON payloads
 app.use(express.json());
+
+//middleware functions to incoming HTTP requests liked with the file js
 app.use('/users', usersRoute);
 
 module.exports = app;
