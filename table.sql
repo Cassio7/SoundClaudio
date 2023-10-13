919d516d32f0a4631beec463765ee9c89b4d54f5
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-13 12:51:47 +0200

    query utopia

--- table.sql
+++ table.sql
@@ -64,27 +64,27 @@ insert into albums(name, id_artist) value('DiCaprio2', 1);
 insert into albums(name, id_artist) value('Utopia', 2);
 
 -- Songs DiCaprio
-insert into songs(name,id_artist,id_album,durata,mp3) value('Working Out',1,1,'3.46','\\mp3\\Workin_Out.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Working Out',1,1,'3.46','../assets/mp3/Working_Out.mp3');
 
 -- Songs Utopia
-insert into songs(name,id_artist,id_album,durata,mp3) value('HYAENA',2,2,'3.42','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('THANK GOD',2,2,'3.04','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('MODERN JAM',2,2,'4.15','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('MY EYES',2,2,'4.11','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('GODS COUNTRY',2,2,'2.07','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('SIRENS',2,2,'3.24','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('MELTDOWN',2,2,'4.06','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('FE!N',2,2,'3.11','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('DELRESTO',2,2,'4.34','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('I KNOW ?',2,2,'3.31','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('TOPIA TWINS',2,2,'3.43','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('CIRCUS MAXIMUX',2,2,'4.18','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('PARASAIL',2,2,'2.34','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('SKITZO',2,2,'6.06','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('LOST FOREVER',2,2,'2.43','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('LOOOVE',2,2,'3.46','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('K-POP',2,2,'3.05','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('TELEKINESIS',2,2,'5.54','');
-insert into songs(name,id_artist,id_album,durata,mp3) value('TIL FURTHER NOTICE',2,2,'5.14','');
+insert into songs(name,id_artist,id_album,durata,mp3) value('HYAENA',2,2,'3.42','../assets/mp3/Utopia/HYAENA.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('THANK GOD',2,2,'3.04','../assets/mp3/Utopia/THANK.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('MODERN JAM',2,2,'4.15','../assets/mp3/Utopia/MODERN.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('MY EYES',2,2,'4.11','../assets/mp3/Utopia/MY.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('GODS COUNTRY',2,2,'2.07','../assets/mp3/Utopia/GODS.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('SIRENS',2,2,'3.24','../assets/mp3/Utopia/SIRENS.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('MELTDOWN',2,2,'4.06','../assets/mp3/Utopia/MELTDOWN.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('FE!N',2,2,'3.11','../assets/mp3/Utopia/FE!N.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('DELRESTO',2,2,'4.34','../assets/mp3/Utopia/DELRESTO.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('I KNOW ?',2,2,'3.31','../assets/mp3/Utopia/KNOW.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('TOPIA TWINS',2,2,'3.43','../assets/mp3/Utopia/TOPIA.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('CIRCUS MAXIMUX',2,2,'4.18','../assets/mp3/Utopia/CIRCUS.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('PARASAIL',2,2,'2.34','../assets/mp3/Utopia/PARASAIL.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('SKITZO',2,2,'6.06','../assets/mp3/Utopia/SKITZO.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('LOST FOREVER',2,2,'2.43','../assets/mp3/Utopia/LOST.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('LOOOVE',2,2,'3.46','../assets/mp3/Utopia/LOOOVE.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('K-POP',2,2,'3.05','../assets/mp3/Utopia/K-POP.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('TELEKINESIS',2,2,'5.54','../assets/mp3/Utopia/TELEKINESIS.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('TIL FURTHER NOTICE',2,2,'5.14','../assets/mp3/Utopia/TIL.mp3');
 
 
