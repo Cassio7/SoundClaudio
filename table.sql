8dfc12579905326a9bbb283b7e4cbb9286b72526
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-13 18:09:46 +0200

    sql

--- table.sql
+++ table.sql
@@ -51,21 +51,42 @@ create table comments(
     FOREIGN KEY (id_user) REFERENCES users(id),
     FOREIGN KEY (id_song) REFERENCES songs(id)
 );
+
 -- The admin and cassio passwords are "Password1*" but with hash
 insert into users(name,email,password,admin) value('Admin','admin@soundclaudio.com','$2b$10$z/jMQ6vU/AuItP/RitMwmeR0J6PBMxXuTuxTmpY.PIBWTuAh4kbD6','1');
 insert into users(name,email,password,admin) value('Cassio','cassio@gmail.com','$2b$10$z/jMQ6vU/AuItP/RitMwmeR0J6PBMxXuTuxTmpY.PIBWTuAh4kbD6','0');
 
 -- Artists
-
 insert into artists(name) value('JID');
 insert into artists(name) value('Travis Scott');
+insert into artists(name) value('More Artists');
+insert into artists(name) value('SZA');
+insert into artists(name) value('Marracash');
 
 -- Albums
-insert into albums(name, img ,id_artist) value('DiCaprio2','', 1);
+insert into albums(name, img ,id_artist) value('The Forever Story','../assets/img/forever.png', 1);
 insert into albums(name, img ,id_artist) value('Utopia','../assets/img/utopia.jpg', 2);
+insert into albums(name, img ,id_artist) value('Users songs','../assets/img/user.png', 3);
+insert into albums(name, img ,id_artist) value('SOS','../assets/img/sos.png', 4);
+insert into albums(name, img ,id_artist) value('Persona','../assets/img/persona.png', 5);
 
--- Songs DiCaprio
-insert into songs(name,id_artist,id_album,durata,mp3) value('Working Out',1,1,'3.46','../assets/mp3/Working_Out.mp3');
+-- Songs Forever
+insert into songs(name,id_artist,id_album,durata,mp3) value('Galaxy',1,1,'','../assets/mp3/Forever/Galaxy.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Raydar',1,1,'','../assets/mp3/Forever/Raydar.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Dance Now',1,1,'','../assets/mp3/Forever/Dance.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Crack Sandwich',1,1,'','../assets/mp3/Forever/Crack.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Cant Punk Me',1,1,'','../assets/mp3/Forever/Cant.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Surround Sound',1,1,'','../assets/mp3/Forever/Surround.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Kody Blu 31',1,1,'','../assets/mp3/Forever/Kody.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Bruddanem',1,1,'','../assets/mp3/Forever/Bruddanem.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Sistanem',1,1,'','../assets/mp3/Forever/Sistanem.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Cant Make U Change',1,1,'','../assets/mp3/Forever/Change.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Stars',1,1,'','../assets/mp3/Forever/Stars.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Just In Time',1,1,'','../assets/mp3/Forever/Just.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Money',1,1,'','../assets/mp3/Forever/Money.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Better Days',1,1,'','../assets/mp3/Forever/Better.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Lauder Too',1,1,'','../assets/mp3/Forever/Lauder.mp3');
+
 
 -- Songs Utopia
 insert into songs(name,id_artist,id_album,durata,mp3) value('HYAENA',2,2,'3.42','../assets/mp3/Utopia/HYAENA.mp3');
@@ -87,3 +108,28 @@ insert into songs(name,id_artist,id_album,durata,mp3) value('LOOOVE',2,2,'3.46',
 insert into songs(name,id_artist,id_album,durata,mp3) value('K-POP',2,2,'3.05','../assets/mp3/Utopia/K-POP.mp3');
 insert into songs(name,id_artist,id_album,durata,mp3) value('TELEKINESIS',2,2,'5.54','../assets/mp3/Utopia/TELEKINESIS.mp3');
 insert into songs(name,id_artist,id_album,durata,mp3) value('TIL FURTHER NOTICE',2,2,'5.14','../assets/mp3/Utopia/TIL.mp3');
+
+--Songs SOS
+insert into songs(name,id_artist,id_album,durata,mp3) value('SOS',4,4,'','../assets/mp3/Utopia/SOS.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Kill Bill',4,4,'','../assets/mp3/Utopia/Kill.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Seek and Destroy',4,4,'','../assets/mp3/Utopia/Seek.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Low',4,4,'','../assets/mp3/Utopia/Low.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Love Language',4,4,'','../assets/mp3/Utopia/Love.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Blind',4,4,'','../assets/mp3/Utopia/Blind.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Used',4,4,'','../assets/mp3/Utopia/Used.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Snooze',4,4,'','../assets/mp3/Utopia/Snooze.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Notice Me',4,4,'','../assets/mp3/Utopia/Notice.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Gone Girl',4,4,'','../assets/mp3/Utopia/Gone.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Smoking on my Ex Pack',4,4,'','../assets/mp3/Utopia/Smoking.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Ghost in the Machine',4,4,'','../assets/mp3/Utopia/Ghost.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('F2F',4,4,'','../assets/mp3/Utopia/F2F.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Nobody Gets Me',4,4,'','../assets/mp3/Utopia/Nobody.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Conceited',4,4,'','../assets/mp3/Utopia/Conceited.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Special',4,4,'','../assets/mp3/Utopia/Special.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Too Late',4,4,'','../assets/mp3/Utopia/Too.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Far',4,4,'','../assets/mp3/Utopia/Far.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Shirt',4,4,'','../assets/mp3/Utopia/Shirt.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Open Arms',4,4,'','../assets/mp3/Utopia/Open.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('I Hate U',4,4,'','../assets/mp3/Utopia/Hate.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Good Days',4,4,'','../assets/mp3/Utopia/Good.mp3');
+insert into songs(name,id_artist,id_album,durata,mp3) value('Forgiveless',4,4,'','../assets/mp3/Utopia/Forgiveless.mp3');
