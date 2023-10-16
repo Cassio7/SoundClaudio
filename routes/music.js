b366d0d56f63af6985c93fe99270edf8fb71d5d3
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-16 21:09:21 +0200

    more info for a song

--- routes/music.js
+++ routes/music.js
@@ -5,7 +5,15 @@ const router = express.Router();
 
 // Get all the album lited with the name of the artist
 router.get('/getallalbums', (req, res) => {
-    query = "select albums.id, albums.img ,albums.name, artists.name as nameart from albums inner join artists on albums.id_artist = artists.id;"
+    const query = `
+        SELECT 
+            albums.id, 
+            albums.img, 
+            albums.name, 
+            artists.name AS nameart
+        FROM albums
+        INNER JOIN artists ON albums.id_artist = artists.id;
+  `;
     connection.query(query, (err, results) => {
         if (err)
             return res.status(400).json(err);
@@ -18,10 +26,21 @@ router.get('/getallalbums', (req, res) => {
         }
     })
 })
-
+// Fetch album, artist, and songs by album ID
 router.post('/:id', (req, res) => {
     const album = req.params.id;
-    query = "select songs.id,songs.name as namesong,albums.img ,albums.name, artists.name as nameart from albums inner join artists on albums.id_artist = artists.id inner join songs on albums.id = songs.id_album where albums.id = ?;"
+    const query = `
+    SELECT 
+        songs.id, 
+        songs.name AS namesong, 
+        albums.img, 
+        albums.name, 
+        artists.name AS nameart
+    FROM albums
+    INNER JOIN artists ON albums.id_artist = artists.id
+    INNER JOIN songs ON albums.id = songs.id_album
+    WHERE albums.id = ?;
+  `;
     connection.query(query, [album], (err, results) => {
         if (err)
             return res.status(400).json(err);
@@ -35,9 +54,28 @@ router.post('/:id', (req, res) => {
     })
 })
 
-router.post('/song/:id', (req,res) =>{
+// Fetch song, comments, and user info by song ID
+router.post('/song/:id', (req, res) => {
     const song = req.params.id;
-    query = "select * from songs where songs.id = ?"
+    query = `  
+        SELECT 
+            comments.id, 
+            comments.comment,
+            comments.id_user,
+            songs.id, 
+            songs.name AS namesong, 
+            albums.img, 
+            albums.name, 
+            artists.name AS nameart, 
+            songs.mp3,
+            users.id,
+            users.name as nameuser
+        FROM albums
+        INNER JOIN artists ON albums.id_artist = artists.id
+        INNER JOIN songs ON albums.id = songs.id_album
+        INNER JOIN comments ON songs.id = comments.id_song
+        INNER JOIN users ON comments.id_user = users.id
+        WHERE songs.id = ?;`
     connection.query(query, [song], (err, results) => {
         if (err)
             return res.status(400).json(err);
@@ -49,6 +87,6 @@ router.post('/song/:id', (req,res) =>{
                 return res.status(400).json({ message: "Nothing found" });
         }
     })
-} )
+})
 
 module.exports = router;
