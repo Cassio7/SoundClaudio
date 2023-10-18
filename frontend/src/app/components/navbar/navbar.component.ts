80356139340805afe09cb87203cfd8a23485e723
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-19 01:29:26 +0200

    navbar user ngif

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
 
+
+
+
+export class NavbarComponent implements OnInit{
+  constructor(private authService: AuthService,
+  ) { }
+  auth = false;
+  ngOnInit(): void {
+    if (this.authService.isAuthenticated()){
+      this.auth = true;
+    }
+      
+  }
 }
