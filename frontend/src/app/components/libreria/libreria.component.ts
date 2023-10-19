2acbd718edbe168fdfc02089e3db838af732f624
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-19 18:41:44 +0200

    need to get id

--- frontend/src/app/components/libreria/libreria.component.ts
+++ frontend/src/app/components/libreria/libreria.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LibraryService } from 'src/app/services/library/library.service';
 
 @Component({
   selector: 'app-libreria',
   templateUrl: './libreria.component.html',
   styleUrls: ['./libreria.component.css']
 })
-export class LibreriaComponent {
+
+export class LibreriaComponent implements OnInit{
+  
+  likes: any;
+  constructor(private libraryServ: LibraryService,
+  ) { }
+
+  ngOnInit(): void {
+    //this.getlikes();
+  }
+
+  // Get all the album from the backend
+  // getlikes(): void {
+  //   this.libraryServ.getlikes(id).subscribe({
+  //     next: (response) => {
+  //       this.likes = response;
+  //     }
+  //   })
+  // }
 
 }
