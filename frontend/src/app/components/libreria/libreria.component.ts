93926723150f24a9d39832f1d2dcefd23638eace
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-20 16:18:53 +0200

    console log

--- frontend/src/app/components/libreria/libreria.component.ts
+++ frontend/src/app/components/libreria/libreria.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { LibraryService } from 'src/app/services/library/library.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+
+// User interface
+interface User {
+  id: number;
+  name: string;
+  admin: any;
+}
 
 @Component({
   selector: 'app-libreria',
@@ -10,20 +18,34 @@ import { LibraryService } from 'src/app/services/library/library.service';
 export class LibreriaComponent implements OnInit{
   
   likes: any;
+  user!: User
+
+  temp: any;
+
   constructor(private libraryServ: LibraryService,
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    //this.getlikes();
+    if (this.authService.isAuthenticated()) {
+      this.temp = this.authService.decode(this.authService.getToken())
+      this.user = {
+        id: this.temp["id"],
+        name: this.temp["name"],
+        admin: this.temp["admin"],
+      }
+      this.getlikes();
+      console.log(this.user.id)
+    }
+    
   }
 
-  // Get all the album from the backend
-  // getlikes(): void {
-  //   this.libraryServ.getlikes(id).subscribe({
-  //     next: (response) => {
-  //       this.likes = response;
-  //     }
-  //   })
-  // }
-
+  //Get all the album from the backend
+  getlikes(): void {
+    this.libraryServ.getlikes(this.user.id).subscribe({
+      next: (response) => {
+        this.likes = response;
+      }
+    })
+  }
 }
