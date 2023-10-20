1cca8a033eef7d66f8d259511e24b54a9276d18a
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-20 15:56:07 +0200

    fix line

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -43,7 +43,6 @@ export class LoginComponent implements OnInit {
           window.location.reload();
         },
         error: (error) => {
-
           alert(error.error.message)
         }
       })
