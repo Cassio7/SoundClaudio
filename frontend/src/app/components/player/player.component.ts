33f083b6864826283418ebd8b0ed97edf90fe829
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-23 18:02:07 +0200

    play function

--- frontend/src/app/components/player/player.component.ts
+++ frontend/src/app/components/player/player.component.ts
@@ -6,5 +6,17 @@ import { Component} from '@angular/core';
   styleUrls: ['./player.component.css']
 })
 export class PlayerComponent{
-
+  audio = new Audio();
+  showRangeInput = false;
+  play():void{
+    if(this.audio.paused){
+      if (this.audio.readyState === 0) {
+        this.audio.src = '../assets/mp3/Forever/Galaxy.mp3';
+      }
+      this.audio.play();
+    }
+    else
+      this.audio.pause()
+    
+  }
 }
