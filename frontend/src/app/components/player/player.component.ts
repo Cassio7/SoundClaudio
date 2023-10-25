718f81603c06fe6bf3c62aa6a390d9642b469606
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-25 11:36:11 +0200

    player

--- frontend/src/app/components/player/player.component.ts
+++ frontend/src/app/components/player/player.component.ts
@@ -1,22 +1,58 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent{
+export class PlayerComponent {
   audio = new Audio();
   showRangeInput = false;
-  play():void{
-    if(this.audio.paused){
+  musicLength: string = '0:00';
+  duration: number = 1;
+  currentTime: string = '0:00';
+
+  constructor() {
+
+    this.audio.ondurationchange = () => {
+      const totalSeconds = Math.floor(this.audio.duration),
+        duration = moment.duration(totalSeconds, 'seconds');
+      this.musicLength = duration.seconds() < 10 ?
+        `${Math.floor(duration.asMinutes())}:
+                          0${duration.seconds()}` :
+        `${Math.floor(duration.asMinutes())}:
+                          ${duration.seconds()}`;
+      this.duration = totalSeconds;
+    }
+
+    this.audio.ontimeupdate = () => {
+      const duration = moment.duration(
+        Math.floor(this.audio.currentTime), 'seconds');
+      this.currentTime = duration.seconds() < 10 ?
+        `${Math.floor(duration.asMinutes())}:
+                          0${duration.seconds()}` :
+        `${Math.floor(duration.asMinutes())}:
+                          ${duration.seconds()}`;
+    }
+  }
+
+  play(): void {
+    if (this.audio.paused) {
       if (this.audio.readyState === 0) {
-        this.audio.src = '../assets/mp3/Forever/Galaxy.mp3';
+        this.audio.src = '../assets/mp3/Forever/Raydar.mp3';
       }
       this.audio.play();
     }
     else
       this.audio.pause()
-    
+
+  }
+
+  volumeSlider(event: any) {
+    this.audio.volume = event.target.value / 100;
+  }
+  durationSlider(event: any) {
+    this.audio.currentTime = event.target.value;
   }
 }
