b2486ae9c01bb04835603404a2b11626d854d5a5
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-26 12:56:52 +0200

    dashboard init

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminService } from 'src/app/services/admin/admin.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  
+  users: any;
+  constructor(private adminServ: AdminService,
+    private router: Router){
 
+  }
+
+  //Get all likes from db, favorite icon change color is match id
+  getall(): void {
+    this.adminServ.getall().subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        if (error.status === 401)
+        // Handle the specific error with status code 404
+        this.router.navigate(['error/401']);
+      }
+    })
+  }
 }
