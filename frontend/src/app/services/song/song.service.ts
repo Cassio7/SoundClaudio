7b45f3fa46baf548546c4dbb120cb835ee4224d3
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-26 18:46:15 +0200

    shuffle songs

--- frontend/src/app/services/song/song.service.ts
+++ frontend/src/app/services/song/song.service.ts
@@ -35,11 +35,11 @@ export class SongService {
 
   // Set the info for player
   setMp3Info(id: number, list: any) {
-    this.mp3Info = { id, list };
+    this.mp3Info = { id, list: [...list] }; // Clone the list, '...' create a shallow copy of an array or an object
   }
 
   // Return function
   getMp3Info(): { id: number, list: any } | null {
-    return this.mp3Info;
+    return this.mp3Info ? { id: this.mp3Info.id, list: [...this.mp3Info.list] } : null; // Clone the list
   }
 }
