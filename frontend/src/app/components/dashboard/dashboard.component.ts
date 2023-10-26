b22a0485027596a9a0b0eb94547eb73235423cc9
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-26 16:55:02 +0200

    frontend admin dash

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { response } from 'express';
 import { AdminService } from 'src/app/services/admin/admin.service';
 
 @Component({
@@ -8,10 +9,18 @@ import { AdminService } from 'src/app/services/admin/admin.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  
+
+  // Print html
   users: any;
+  albums: any;
+  songs: any;
+  // Flags for show
+  flaguser: boolean = false;
+  flagalbum: boolean = false;
+  flagsong: boolean = false;
+
   constructor(private adminServ: AdminService,
-    private router: Router){
+    private router: Router) {
 
   }
 
@@ -20,11 +29,86 @@ export class DashboardComponent {
     this.adminServ.getall().subscribe({
       next: (response) => {
         this.users = response;
+        this.flaguser = !this.flaguser ? true : false;
       },
       error: (error) => {
         if (error.status === 401)
-        // Handle the specific error with status code 404
-        this.router.navigate(['error/401']);
+          // Handle the specific error with status code 401
+          this.router.navigate(['error/401']);
+      }
+    })
+  }
+
+  // Call api to delete user, pass id
+  delete(id: number): void {
+    this.adminServ.delete(id).subscribe({
+      next: (response) => {
+        let mes: any = response
+        alert(mes.message + ' user id: ' + id)
+        window.location.reload();
+      }
+    })
+  }
+
+  // Call api to delete user, pass id
+  promote(id: number): void {
+    this.adminServ.promote(id).subscribe({
+      next: (response) => {
+        let mes: any = response
+        alert(mes.message + ' user id: ' + id)
+        window.location.reload();
+      }
+    })
+  }
+
+  //Get all albums 
+  getallalbums(): void {
+    this.adminServ.getallalbums().subscribe({
+      next: (response) => {
+        this.albums = response;
+        this.flagalbum = !this.flagalbum ? true : false;
+      },
+      error: (error) => {
+        if (error.status === 401)
+          // Handle the specific error with status code 401
+          this.router.navigate(['error/401']);
+      }
+    })
+  }
+
+  // Call api to delete album, pass id
+  deletealbums(id: number): void {
+    this.adminServ.deletealbums(id).subscribe({
+      next: (response) => {
+        let mes: any = response
+        alert(mes.message + ' album id: ' + id)
+        window.location.reload();
+      }
+    })
+  }
+
+  //Get all sogns 
+  getallsongs(): void {
+    this.adminServ.getallsongs().subscribe({
+      next: (response) => {
+        this.songs = response;
+        this.flagsong = !this.flagsong ? true : false;
+      },
+      error: (error) => {
+        if (error.status === 401)
+          // Handle the specific error with status code 401
+          this.router.navigate(['error/401']);
+      }
+    })
+  }
+
+  // Call api to delete song, pass id
+  deletesong(id: number): void {
+    this.adminServ.deletesong(id).subscribe({
+      next: (response) => {
+        let mes: any = response
+        alert(mes.message + ' song id: ' + id)
+        window.location.reload();
       }
     })
   }
--- frontend/src/app/services/admin/admin.service.ts
+++ frontend/src/app/services/admin/admin.service.ts
@@ -13,9 +13,68 @@ export class AdminService {
   constructor(private httpClient: HttpClient,
     private authServ: AuthService) { }
 
+  // Get all users from api, for all this function set a token on header
   getall() {
     return this.httpClient.get(this.url + "/admin/getall", {
-      headers: new HttpHeaders().set("Authorization", "Bearer "+this.authServ.getToken())
+      headers: new HttpHeaders().set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Delete user from db by id
+  delete(id: number) {
+    const requestBody = {
+      id: id
+    };
+    return this.httpClient.post(this.url + "/admin/delete", requestBody, {
+      headers: new HttpHeaders().set("Content-Type", "application/json")
+        .set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Delete user from db by id
+  promote(id: number) {
+    const requestBody = {
+      id: id
+    };
+    return this.httpClient.post(this.url + "/admin/promote", requestBody, {
+      headers: new HttpHeaders().set("Content-Type", "application/json")
+        .set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Get all albums from api
+  getallalbums() {
+    return this.httpClient.get(this.url + "/admin/getallalbums", {
+      headers: new HttpHeaders().set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Delete album from db by id
+  deletealbums(id: number) {
+    const requestBody = {
+      id: id
+    };
+    return this.httpClient.post(this.url + "/admin/deletealbums", requestBody, {
+      headers: new HttpHeaders().set("Content-Type", "application/json")
+        .set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Get all songs from api
+  getallsongs() {
+    return this.httpClient.get(this.url + "/admin/getallsongs", {
+      headers: new HttpHeaders().set("Authorization", "Bearer " + this.authServ.getToken())
+    })
+  }
+
+  // Delete song from db by id
+  deletesong(id: number) {
+    const requestBody = {
+      id: id
+    };
+    return this.httpClient.post(this.url + "/admin/deletesong", requestBody, {
+      headers: new HttpHeaders().set("Content-Type", "application/json")
+        .set("Authorization", "Bearer " + this.authServ.getToken())
     })
   }
 }
