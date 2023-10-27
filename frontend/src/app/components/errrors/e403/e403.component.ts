718275a583a766a1ddb3c6e53406ea1c09a4cd60
Cassio7 <Cassio7@users.noreply.github.com>
2023-10-27 15:37:08 +0200

    error 403

---
+++ frontend/src/app/components/errrors/e403/e403.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e403',
+  templateUrl: './e403.component.html',
+  styleUrls: ['./e403.component.css']
+})
+export class E403Component {
+
+}
